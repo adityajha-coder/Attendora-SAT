c50f5acff08d71f74d1ab3c32f8be63287876c16
Aditya Jha <adityajha97172@gmail.com>
2026-07-27 20:42:44 +0530

    update files

--- api/index.mjs
+++ api/index.mjs
@@ -1,3 +1,7 @@
-import app from '../server/server.mjs';
+import app from '../server/app.mjs';
+import { connectDatabase } from '../server/db/connection.mjs';
+
+// Connect DB for serverless (Vercel) environment
+connectDatabase();
 
 export default app;
